690599eace6a5eec82c12c6e13d4ad0506be3ae4
Aatik Ali Makani <aatikali99@gmail.com>
2024-05-13 14:05:27 -0500

    Update rq7.py

--- random_queries/rq7.py
+++ random_queries/rq7.py
@@ -9,5 +9,16 @@
 #     print (siuuu,end = " ")
 
 
-for num in range (0,9,2):
-    print(num,end = " ")
+# for num in range (0,9,2):
+#     print(num,end = " ")
+
+# for num in range (0,24,3):
+#     print(num,end = " ")
+
+# for num in range (3,12,5):
+#     print(num,end = " ")
+
+count = 0
+for num in range (5,25,2):
+    count += 1
+print(count,end = " ")
