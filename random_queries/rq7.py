7f650960ba5281a69ed76df95247157e2c842fb7
Aatik Ali Makani <aatikali99@gmail.com>
2024-05-13 13:55:21 -0500

    Update rq7.py

--- random_queries/rq7.py
+++ random_queries/rq7.py
@@ -5,5 +5,9 @@
 # for value in range (0,2):
 #     print (num,end = " ")
 
-for siuuu in range (3,13):
-    print (siuuu,end = " ")
+# for siuuu in range (3,13):
+#     print (siuuu,end = " ")
+
+
+for num in range (0,10,2):
+    print(num,end = " ")
