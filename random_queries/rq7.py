adadb8618fa54edb5aab6d22057e2a75cb6d2015
Aatik Ali Makani <aatikali99@gmail.com>
2024-05-17 22:40:37 -0500

    Update rq7.py

--- random_queries/rq7.py
+++ random_queries/rq7.py
@@ -1,56 +1,56 @@
-# for num in range (0,10):
-#     print(num,end = " ")
+for num in range (0,10):
+    print(num,end = " ")
 
 
-# for value in range (0,2):
-#     print (num,end = " ")
+for value in range (0,2):
+    print (num,end = " ")
 
-# for siuuu in range (3,13):
-#     print (siuuu,end = " ")
-
-
-# for num in range (0,9,2):
-#     print(num,end = " ")
-
-# for num in range (0,24,3):
-#     print(num,end = " ")
-
-# for num in range (3,12,5):
-#     print(num,end = " ")
-
-# count = 0
-# for num in range (5,25,2):
-#     count += 1
-#     print(num,end = " ")
-# print ()
-# print(count,end = " ")
+for siuuu in range (3,13):
+    print (siuuu,end = " ")
 
 
-# num = 2
-# while num < 12:
-#     num += 1
-#     print(num,end = " ")
+for num in range (0,9,2):
+    print(num,end = " ")
 
-# total = 0
-# for num in range (1,6):
-#     total += num
-# print (total)
+for num in range (0,24,3):
+    print(num,end = " ")
 
-# for x in range (6,-2,-1):
-#     print(x,end = " ")
+for num in range (3,12,5):
+    print(num,end = " ")
 
-# num=30
-# while num >4:
-#     print(num)
-#     num = num/2
+count = 0
+for num in range (5,25,2):
+    count += 1
+    print(num,end = " ")
+print ()
+print(count,end = " ")
 
-# for x in range (11,-4,-2):
-#     print (x,end=" ")
 
-# total = 0
-# for num in range (1,15):
-#     total = total + num
-# print (total)
+num = 2
+while num < 12:
+    num += 1
+    print(num,end = " ")
+
+total = 0
+for num in range (1,6):
+    total += num
+print (total)
+
+for x in range (6,-2,-1):
+    print(x,end = " ")
+
+num=30
+while num >4:
+    print(num)
+    num = num/2
+
+for x in range (11,-4,-2):
+    print (x,end=" ")
+
+total = 0
+for num in range (1,15):
+    total = total + num
+print (total)
 
 total=0
 num = 0
