8ea8f8097dad54224016fc60b6d8756862339f9e
Aatik Ali Makani <aatikali99@gmail.com>
2024-06-03 14:58:16 -0500

    updates

--- assignments/Glossary/Makani-assign6.py
+++ assignments/Glossary/Makani-assign6.py
@@ -1,33 +1,38 @@
+import code 
 glossary = {}
 
 def getFileText(fname):
     
     try: 
-        #your code here
+        infile = open(fname,"r")
+        lineList=infile.readlines()
+        infile.close()
+        return (lineList)
     except:
         print(f"{fname} could not be opened.")
-        return []    
+        return []  
     
     
 
 
-def glossaryTerms(fname):
-    tlst = getFileText(fname)
-    #your code here
+# def glossaryTerms(fname):
+#     tlst = getFileText(fname)
+#     #your code here
     
     
-    printWordD(dictionary)
+#     printWordD(dictionary)
 
 
-def findTerm(key):
+# def findTerm(key):
     
-    #your code here
-    print("your key-value pair")
+#     #your code here
+#     print("your key-value pair")
     
     
 
-def printWordD(d):
-    for word, freq in d.items():
-        print(word + ':' + freq)
+# def printWordD(d):
+#     for word, freq in d.items():
+#         print(word + ':' + freq)
         
-        
+
+code.interact(local=dict(globals(), **locals()))
